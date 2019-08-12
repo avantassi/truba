5c90cde03e19262176f8778fab73b32a75c0e7db
Екатерина Савина <esavina@bkcco.ru>
2019-08-12 14:07:59 +0300

    changed form

--- js/main.js
+++ js/main.js
@@ -31,23 +31,6 @@ document.addEventListener("DOMContentLoaded", function(){
     });
 });
 
-$(document).ready(function() {
-
-    //E-mail Ajax Send
-    $("form").submit(function() { //Change
-        var th = $(this);
-        $.ajax({
-            type: "POST",
-            url: "mail.php", //Change
-            data: th.serialize()
-        }).done(function() {
-            alert("Thank you!");
-            setTimeout(function() {
-                // Done Functions
-                th.trigger("reset");
-            }, 1000);
-        });
-        return false;
-    });
-
+$('form.example').sendMail({
+    // Параметры...
 });

f5f6abe11c990d2689591d01fcc99b48ceb7f417
Екатерина Савина <esavina@bkcco.ru>
2019-08-12 13:55:15 +0300

    added form

--- js/main.js
+++ js/main.js
@@ -12,6 +12,12 @@ $('.flowing-scroll').on( 'click', function(){
     return false;
 });
 
+$(function(){
+    $("#phone").mask("+7-999-999-99-99");
+    $("#phone2").mask("+7-999-999-99-99");
+    $("#phone3").mask("+7-999-999-99-99");
+});
+
 document.addEventListener("DOMContentLoaded", function(){
     var scrollbar = document.body.clientWidth - window.innerWidth + 'px';
     console.log(scrollbar);
@@ -23,4 +29,25 @@ document.addEventListener("DOMContentLoaded", function(){
         document.body.style.overflow = 'visible';
         // document.querySelector('#openModal').style.marginLeft = '0px';
     });
+});
+
+$(document).ready(function() {
+
+    //E-mail Ajax Send
+    $("form").submit(function() { //Change
+        var th = $(this);
+        $.ajax({
+            type: "POST",
+            url: "mail.php", //Change
+            data: th.serialize()
+        }).done(function() {
+            alert("Thank you!");
+            setTimeout(function() {
+                // Done Functions
+                th.trigger("reset");
+            }, 1000);
+        });
+        return false;
+    });
+
 });
